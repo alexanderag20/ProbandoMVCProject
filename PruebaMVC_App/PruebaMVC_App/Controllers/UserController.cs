052c954694d541df4564921829b25f0311e92ae8
alexanderag20 <alexariasg20@gmail.com>
2018-10-08 00:29:05 -0400

    Adding all Project Files.

---
+++ PruebaMVC_App/PruebaMVC_App/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PruebaMVC_App.Models;
+
+namespace PruebaMVC_App.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult AddOrEdit(int id=0)
+        {
+            User userModel = new User();
+
+            return View(userModel);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit (User userModel)
+        {
+            
+            using (AppMVCEntitiesDB dbModel = new AppMVCEntitiesDB())
+            {     
+                dbModel.Users.Add(userModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful";
+            return View("AddOrEdit", new User());
+        }
+    }
+}
---
+++ PruebaMVC_App/PruebaMVC_App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PruebaMVC_App.MvcApplication" Language="C#" %>
